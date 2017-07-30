8ed13909ef0b039ba922c43f22772036c7d23337
Tono <antonio.nogueras@yahoo.com>
2017-07-30 13:27:36 -0500

    Create main-two.js

---
+++ main-two.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.btn').click(function(){
+     $('.hoursLeft').toggleClass('unColor');
+  });
+
+  $('.footer .logo--small').click(function(){
+     $('.project-container').toggleClass('byebye');
+  });
+});
