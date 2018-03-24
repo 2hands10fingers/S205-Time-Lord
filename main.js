ea7ea550bb5e6dd5b14c32c3c8b4920b355eefe5
Tono <antonio.nogueras@yahoo.com>
2018-03-24 15:13:36 -0500

    JS update to add new features and refactor all the code

--- main.js
+++ main.js
@@ -1,122 +1,202 @@
 $(document).ready(function() {
 
-  // LOADING
-  $(function() {
-    $('.loading_wrap').fadeOut(3000, function() {
-      $('#mega-container').fadeIn(2000);
-      $('body').addClass('makeSquareGreen');
+    // LOADING
+    $(function() {
+      $('.loading_wrap').fadeOut(1000, function() {
+        $('#mega-container').fadeIn(2000);
+        $('body').addClass('makeSquareGreen');
+      });
     });
-  });
 
-// LAZY GLOBAL VARIABLES
-  var projectCont = $('.project-container');
+    // GLOBAL VARIABLES
+    var projectCont = $('.project-container');
+    var company = '';
+    var key = '';
 
-  // HEADERS
-    var company = "square205";
-    var key = "";
-
-// FUNCTIONS & LOOPS
-    // LOOP FOR TIME TOTALS AJAX CALL
-    $.getJSON('serverside.php', function (projArray) {
-      for(var i = 0; i < projArray.length; i++){
-          $.ajax({
-            url: 'https://' + company + '.teamwork.com/projects/' + projArray[i].projID + '/time/total.json?callback=?',
-            headers: {"Authorization": "BASIC " + window.btoa(key + ":xxx")},
-            dataType: 'JSONP',
-            application: "application/json",
-            async: false,
-            context: projArray[i],
-            success: timeCallback
-          });
-        }
-    })
-
-    // CREATE DATE PASSED IN, PARSE IT, MODIFY, AND ADD IT TO SPECIFIC SPAN CLASS
-    function deadlineCallback(deadline) {
-      var datePassed = deadline.project.endDate;
-      var projIDvar = "#" + this.projID;
-      var thisProj = this.projID;
-      var newDateString = datePassed.substr(4,2)  + " " + datePassed.substr(6,2) + " " + datePassed.substr(0, 4);
-      
-      // CREATE THE DATE. FORMATTING SUCKS.
-      projectCont.find("#" + this.projID + " .month").append(datePassed.substr(4,2));
-      projectCont.find("#" + this.projID + " .day").append(datePassed.substr(6,2));
-      projectCont.find("#" + this.projID + " .year").append(datePassed.substr(0,4));
-
-      // APPENDS TEAMWORK TAGS
-      var tagLocaleOne = deadline.project.tags[0]["name"];
-      var tagLocaleTwo = deadline.project.tags[1]["name"];
-
-      projectCont.find("#" + this.projID + " .firstTag").append(tagLocaleOne);
-      projectCont.find("#" + this.projID + " .projectAssigned").append(tagLocaleTwo);
-      projectCont.find("#" + this.projID + " .idDisplay").append(thisProj);
-      
-      // COLOR TAGS APPROPRIATELY
-      if (tagLocaleOne === "Billable") {
-        projectCont.find("#" + this.projID + " .firstTag").addClass('billable');
-      } else if (tagLocaleOne === "Contract") {
-        projectCont.find("#" + this.projID + " .firstTag").addClass('contract');
+    function loopLog(array) {
+      for (var i = 0; i < array.length; i++) {
+        // console.log(array[i]);
       }
+    }
 
-  }
+    function dateAssign(proj, dateClass, theDate ,firstNum, secondNum) {
+      projectCont.find("#" + proj + dateClass).append(theDate.substr(firstNum, secondNum));
+    }
 
-    // CREATES MAIN DATA ITEMS TO BE DISPLAYED IN HTML AND LOOPS THROUGH DEADLINES
-    function timeCallback(data) {
-      var totalHoursSum = data["projects"][0]["time-totals"]["total-hours-sum"];
-      var projName = data["projects"][0]["name"];
-      var totalHoursLeft = this.budget - totalHoursSum;
-   
-    // BUILDS THE TABLE ROW
-      var builder = '<tr id ="' + this.projID + '" class="item">'
-      builder += '<td class="projName project dividers">'
-      builder += projName + ' <a target="_blank" href="https://square205.teamwork.com/projects/' + this.projID + '/time">&#10140;</a>' + '</br><span class="firstTag">' + '</span><span class="idDisplay">ID#:</span>'
-      builder += '<td class="projectAssigned"></td>'
-      builder += '<td class="hoursBudgeted">'
-      builder += this.budget
-      builder += '</td>'
-      builder += '<td class="hoursEntered">'
-      builder += totalHoursSum
-      builder += '</td>'
-      builder += '<td class="hoursLeft">'
-      builder += parseInt(totalHoursLeft)
-      builder += '</td>'
-      builder += '<td class="deadline">'
-      builder += '<span class="month"></span>/<span class="day"></span>/<span class="year"></span>'
-      builder += '</td>'
-      builder += '</tr>'
-      console.log(projName)
-      projectCont.append(builder);
+    function generalAppender(theID, theClass, appendThis ) {
+      projectCont.find("#" + theID + theClass).append(appendThis);
+      // console.log("#" + theID + theClass);
+    }
 
-      if (totalHoursSum < this.budget * 0.25 && totalHoursSum >= this.budget * 0) {
-        projectCont.find("#" + this.projID + ' .hoursLeft').addClass('makeGreen');
-      } else if (totalHoursSum >= this.budget * 0.25 && totalHoursSum <= this.budget * 0.50) {
-        projectCont.find("#" + this.projID + ' .hoursLeft').addClass('makeYellow');
-      } else if (totalHoursSum < this.budget * 0.75 && totalHoursSum > this.budget * 0.50) {
-        projectCont.find("#" + this.projID + ' .hoursLeft').addClass('makeOrange');
-      } else if (totalHoursSum < this.budget * 0.90 && totalHoursSum > this.budget * 0.75) {
-        projectCont.find("#" + this.projID + ' .hoursLeft').addClass('makeBurntOrange');
-      } else if (totalHoursSum < this.budget && totalHoursSum >= this.budget * 0.90) {
-        projectCont.find("#" + this.projID + ' .hoursLeft').addClass('makeRed');
-      } else if (totalHoursSum > this.budget) {
-        projectCont.find("#" + this.projID + ' .hoursLeft').addClass('makeDarkRed');
-      }
-
-      // SECONDARY AJAX CALL FOR DEADLINES
+    function ajaxCaller(queryString, theContext, successFunction) {
       $.ajax({
-        url: 'https://' + company + '.teamwork.com/projects/' + this.projID + '.json?callback=?',
-        headers: {"Authorization": "BASIC " + window.btoa(key + ":xxx")},
-        dataType: 'JSON',
-        application: "application/json",
-        async: false,
-        context: this,
-        success: deadlineCallback
+          url: 'https://' + company + '.teamwork.com' + queryString,
+          headers: {"Authorization": "BASIC " + window.btoa(key + ":xxx")},
+          dataType: 'JSON',
+          application: "application/json",
+          async: true,
+          context: theContext,
+          success: successFunction
       });
     }
 
-    // HARD REFRESHES EVERY HOUR
-    setInterval(function(){cache_clear()},3600000);
     function cache_clear() {
       window.location.reload(true);
       // window.location.reload(); use this if you do not remove cache
       }
+
+    // CREATE DATE PASSED IN, PARSE IT, MODIFY, AND ADD IT TO SPECIFIC SPAN CLASS
+    function deadlineCallback(deadline) {
+      var datePassed = deadline.project.endDate;
+      var tags = deadline.project.tags
+      var thisProj = this.projID;
+
+      dateAssign(thisProj, " .month", datePassed, 4, 2)
+      dateAssign(thisProj, " .day", datePassed, 6, 2)
+      dateAssign(thisProj, " .year", datePassed, 0, 4)
+
+      for(var i = 0; i < tags.length; i++) {
+        tagName = tags[i]["name"]
+
+        if (tagName === "PM: Roger")        { generalAppender(thisProj, " .projectAssigned", "R.W.") }
+        else if (tagName === "PM: Charles") { generalAppender(thisProj, " .projectAssigned", "C.M.") }
+        else if (tagName === "PM: Jim")     { generalAppender(thisProj, " .projectAssigned", "J.S.") }
+        else                                { generalAppender(thisProj, " .projectAssigned", "") }
+      }
+    }
+
+
+  function taskListCallback(taskLists) {
+    var listHeiarchy = []
+    // console.log(taskLists);
+
+    for (var i = 0; i < taskLists.tasklists.length; i++) {
+      var base = taskLists.tasklists[i];
+      var listName = base["name"]
+      var uncompletedCount = base["uncompleted-count"]
+      var projID = base["projectId"]
+      var msData = base["milestone"]
+
+      if (msData == undefined) {
+          msData =  { "deadline" : "0000-00-00T00:00:00Z" }
+        } else {
+          listName = base["milestone"]["title"];
+        }
+
+      var taskListsObj = {
+                           "project" : listName,
+                           "count" : uncompletedCount,
+                           "id": projID,
+                           "milestoneData": msData
+                         }
+
+      console.log(taskListsObj);
+      listHeiarchy.push(taskListsObj)
+    }
+
+    for (var i = 0; i < listHeiarchy.length; i++) {
+        var listHeiarchyBase = listHeiarchy[i]
+        var projCount = listHeiarchyBase["count"]
+        var projNameList = listHeiarchyBase["project"]
+        var projNameID = listHeiarchyBase["id"]
+        var projMSDeadline = listHeiarchyBase["milestoneData"]["deadline"]
+
+        if (projCount == 0 || projNameList == "Project Management" || projNameList == "Project Onboarding" || projNameList == "Project Kickoff") {
+          // pass
+        } else {
+          dateAssign(parseInt(projNameID), " .ms-month", projMSDeadline, 5, 2)
+          dateAssign(parseInt(projNameID), " .ms-day", projMSDeadline, 8, 2)
+          dateAssign(parseInt(projNameID), " .ms-year", projMSDeadline, 0, 4)
+          generalAppender(projNameID, " .current-tasklist", projNameList)
+          generalAppender(projNameID, " .task-count", projCount)
+          break
+        }
+      }
+    }
+
+    function selectorBuilder(data) {
+      for (var i = 0; i < data.projects.length; i++) {
+        var projName = data["projects"][i].name;
+        var projID = data["projects"][i].id;
+        var sBuilder = '<option value="'+ projID +'">'+ projName  +'</option>';
+
+        $('.selector').append(sBuilder)
+
+        }
+    }
+
+    // CREATES MAIN DATA ITEMS TO BE DISPLAYED IN HTML AND LOOPS THROUGH DEADLINES
+    function timeCallback(data) {
+      var projID = this.projID
+      var budget = this.budget
+      var projects = data["projects"];
+      var totalHoursSum = data["projects"][0]["time-totals"]["total-hours-sum"];
+      var projName = data["projects"][0]["name"];
+      var totalHoursLeft = budget - totalHoursSum;
+
+      var builder = '<tr id ="' + projID + '" class="item">'
+
+      builder += '<td class="projectAssigned"></td>'
+
+      builder += '<td class="projName project dividers">'
+      builder +=  '<a target="_blank" href="https://square205.teamwork.com/projects/' + projID + '/time">' + projName +'</a>'
+      builder += '</td>'
+
+      builder += '<td class="tasklists">'
+      builder += '<p class="current-tasklist"></p>'
+      builder += '<span class="tasks-left">Tasks Left: </span><span class="tasks-left task-count"></span>'
+      builder += '</td>'
+
+      builder += '<td class="ms-deadline">'
+      builder += '<span class="ms-month"></span>.<span class="ms-day"></span>.<span class="ms-year"></span>'
+      builder += '</td>'
+
+      builder += '<td class="deadline">'
+      builder += '<span class="month"></span>.<span class="day"></span>.<span class="year"></span>'
+      builder += '</td>'
+
+      builder += '<td class="hoursBudgeted">'
+      builder += budget
+      builder += '</td>'
+
+      builder += '<td class="hoursEntered">'
+      builder += totalHoursSum
+      builder += '</td>'
+
+      builder += '<td class="hoursLeft">'
+      builder += parseInt(totalHoursLeft)
+      builder += '</td>'
+
+      builder += '</tr>'
+
+      projectCont.append(builder);
+
+      function colorAssign(colorClass) { projectCont.find("#"+projID+' .hoursLeft').addClass(colorClass); }
+
+      if (totalHoursSum <= budget * 0.50 && totalHoursSum >= 0)                 { colorAssign('makeGreen'); }
+      else if (totalHoursSum > budget * 0.50 && totalHoursSum <= budget * 0.75) { colorAssign('makeYellow'); }
+      else if (totalHoursSum > budget || totalHoursSum > budget * 0.75 )        { colorAssign('makeRed'); }
+
+      ajaxCaller( '/projects/' +  projID + '.json?callback=?', this,  deadlineCallback)
+      ajaxCaller('/projects/' + projID + '/tasklists.json?showMilestones=1', "", taskListCallback)
+    }
+
+    // LOOP FOR TIME TOTALS AJAX CALL
+    $.getJSON('/php/serverside.php', function (projArray) {
+      for(var i = 0; i < projArray.length; i++){
+
+        ajaxCaller('/projects/' + projArray[i].projID + '/time/total.json?callback=?',
+                    projArray[i],
+                    timeCallback)
+        }
+    });
+
+    // FILLS IN SELECTORS
+    ajaxCaller('/projects.json?callback=?', "", selectorBuilder)
+
+    // HARD REFRESHES EVERY HOUR
+    setInterval(function() {
+      cache_clear()
+    }, 3600000);
+
 });
