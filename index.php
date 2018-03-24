ef4e2fc25e62487f90812e02133a73b1a9a0c312
Tono <antonio.nogueras@yahoo.com>
2018-03-24 15:08:13 -0500

    Update to the HTML

--- index.php
+++ index.php
@@ -1,105 +1,294 @@
 <!DOCTYPE html>
 <html>
+
   <head>
+    <meta charset="UTF-8">
     <title>SQUARE 205 T I M E L O R D</title>
+    <base href="#">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://use.typekit.net/cbb3yyn.js"></script>
     <script>try{Typekit.load({ async: true });}catch(e){}</script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="shortcut icon" href="IMG/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="IMG/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.js"></script>
-    <script src="main.js" charset="utf-8"></script>
-    <script src="main-two.js" charset="utf-8"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <script src="sorttable.js" charset="utf-8"></script>
-    <link rel="stylesheet" type="text/css" href="loadingscreen.css">
+    <script src="JS/main.js" charset="utf-8"></script>
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
+    <script src="JS/sorttable.js" charset="utf-8"></script>
+    <link rel="stylesheet" type="text/css" href="CSS/loadingscreen.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
   </head>
+
 <body>
   <div class="loading_wrap" style="position:fixed; height:100%; width:100%; overflow:hidden; top:0; left:0;">
-    <!--   Loading Graphic here -->
+    <div class="socket">
+    <div class="gel center-gel">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c1 r1">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c2 r1">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c3 r1">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c4 r1">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c5 r1">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c6 r1">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+
+    <div class="gel c7 r2">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+
+    <div class="gel c8 r2">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c9 r2">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c10 r2">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c11 r2">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c12 r2">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c13 r2">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c14 r2">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c15 r2">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c16 r2">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c17 r2">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c18 r2">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c19 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c20 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c21 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c22 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c23 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c24 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c25 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c26 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c28 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c29 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c30 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c31 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c32 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c33 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c34 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c35 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c36 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+    <div class="gel c37 r3">
+      <div class="hex-brick h1"></div>
+      <div class="hex-brick h2"></div>
+      <div class="hex-brick h3"></div>
+    </div>
+
+  </div>
+  <img class="loader-logo" src="http://square205.com/wp-content/themes/square205/images/logo-white.png" alt="">
 </div>
 
+
 <div id="mega-container">
-  <!--  ADD ROW FORM -->
-  <div style="display: none;max-width:600px;" id="hidden-content-d">
+  <div style="display: none; max-width:600px;" id="hidden-content-d">
         <h2>Enter Your Project</h2>
         <h6>...it couldn't hurt!</h6>
-    <form action="process.php" method="post">
-      <span>Project ID</span><input type="text" class="projInput" name="projID">
-        </br>
+    <form action="/php/process.php" method="post">
+      <span>Project ID</span>
+      <select class="selector" name="projID">
+        <!-- options go here -->
+      </select>
+        <br>
       <span>Budgeted Hours</span> <input type="text" class="projInput" name="budget">
-        </br>
+        <br>
     <div class="button-form-container">
         <input class="subBtn" type="submit">
         <button data-fancybox-close class="closeBtn">Cancel</button>
       </div>
     </form>
-      <img class="logo--small" src="logo-small.png" alt="small s205 logo"/>
+      <img class="logo--small" src="/IMG/logo-small.png" alt="small s205 logo"/>
   </div>
-<!-- REMOVE ROW FORM -->
-  <div style="display: none;max-width:600px;" id="hidden-content-c">
+
+  <div style="display: none; max-width:600px;" id="hidden-content-c">
       <h2>Remove A Project</h2>
       <h6>...it should feel satisfying</h6>
-    <form action="delete.php" method="post">
-      <span>Project ID</span> <input type="text" class="projInput" name="RprojID">
-        </br>
+    <form action="/php/delete.php" method="post">
+      <span>Project ID</span>
+
+      <select class="selector top" name="RprojID">
+        <!-- selections go here -->
+      </select>
+        <br>
       <div class="button-form-container">
         <input class="subBtn" type="submit">
         <button data-fancybox-close class="closeBtn">Cancel</button>
       </div>
    </form>
-      <img class="logo--small" src="logo-small.png" alt="small s205 logo"/>
+      <img class="logo--small" src="IMG/logo-small.png" alt="small s205 logo"/>
   </div>
-<span class="timeLordTitle">
-  <img class="logo" src="http://square205.com/wp-content/themes/square205/images/logo-white.png" alt=""><strong><span>&nbsp; T I M E  &nbsp; L O R D v1.0</span></strong></span>
-<div class="btn">
-    <div class="hexagon">
-      <div class="hexTop"></div>
-      <div class="hexBottom"></div>
-    </div>
-</div>
-</br>
-<div id="tools">
-  <div id="key">
-    <ul>
-      <li><span class="box makeGreen"></span> 0-25%</li>
-      <li><span class="box makeYellow"></span> 25-50%</li>
-      <li><span class="box makeOrange"></span> 50-75%</li>
-      <li><span class="box makeBurntOrange"></span> 75-90%</li>
-      <li><span class="box makeRed"></span> 90-100%</li>
-      <li><span class="box makeDarkRed"></span> >100%</li>
-  </div>
-  <div class="addRow-btn">
-    <span>
-      <a data-fancybox data-options='{"src": "#hidden-content-d", "modal": true}' href="javascript:;" > &#65291; Row</a>
-    </span>
-  </div>
-  <div class="minusRow-btn">
-    <span>
-      <a data-fancybox data-options='{"src": "#hidden-content-c", "modal": true}' href="javascript:;" > - Row</a>
-    </span>
-  </div>
-</div>
+
+
 <div class="dataHere">
   <table class="sortable">
   <thead class="header">
     <tr class="item totals">
-      <th class="project">Project Title</th>
-      <th class="project-manager">Project Manager</th>
-      <th class="sorttable_numeric">Hours Budgeted</th>
-      <th class="sorttable_numeric">Hours Entered</th>
-      <th class="sorttable_numeric">Hours Left</th>
-      <th>Deadline</th>
+      <th class="project-manager">P.M.</th>
+      <th class="project">Project</th>
+      <th class="deadline--header sorttable_numeric">Deadline</th>
+      <th class="hoursEntered--header sorttable_numeric">Hrs. Entered</th>
+      <th class="totalHours--header sorttable_numeric">Total Hrs.</th>
+      <th class="hoursLeft--header">Hrs. Left</th>
     </tr>
   </thead>
   <tbody class="project-container">
+
       <!-- TABLES ROWS WILL GO HERE -->
-</tbody>
+
+  </tbody>
 </table>
-  <div class="footer">
-    <img class="logo--small" src="logo-small.png" alt="small s205 logo"/>
+  
+    <div class="adminButtons">
+      <div class="addRow-btn">
+        <span>
+          <a data-fancybox data-options='{"src": "#hidden-content-d", "modal": true}' href="javascript:;" > &#65291;</a>
+        </span>
+      </div>
+
+      <div class="minusRow-btn">
+        <span>
+          <a data-fancybox data-options='{"src": "#hidden-content-c", "modal": true}' href="javascript:;" > - </a>
+        </span>
+      </div>
+    </div>
+
+    </div>
   </div>
-</div>
-</div>
-</body>
+
+  </body>
 </html>
