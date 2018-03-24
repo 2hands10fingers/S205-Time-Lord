2799261376eb5336fce2e4bf628230ebf64724d2
Tono <antonio.nogueras@yahoo.com>
2018-03-24 15:19:42 -0500

    Delete main-two.js

--- main-two.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('.btn').click(function(){
-     $('.hoursLeft').toggleClass('unColor');
-  });
-
-  $('.footer .logo--small').click(function(){
-     $('.project-container').toggleClass('byebye');
-  });
-});
